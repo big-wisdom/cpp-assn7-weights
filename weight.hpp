3f42f28da103397cc214d8e6f341ab893e990664
Eli Hermann <Eli_Hermann@hotmail.com>
2021-11-20 19:38:23 -0700

    okay I got all the errors to go away in the main file

--- weight.hpp
+++ weight.hpp
@@ -3,19 +3,103 @@
 namespace usu
 {
     template <typename Rep, typename W = std::ratio<1>>
-    class weight
+    class Weight
     {
         public:
-            weight(Rep newCount) : count(newCount) {};
+            Weight(Rep newCount) : number(newCount) {};
 
-            W weightRatio;
-            Rep count;
+            W WeightRatio;
+            Rep number;
+
+            Rep count()
+            {
+                return number;
+            }
+
+            template <typename S>
+            Weight& operator+(S s)
+            {
+                Weight<double> newS(1000);
+                return newS;
+            }
+
+            template <typename S>
+            Weight& operator-(S s)
+            {
+                Weight<double> newS(1000);
+                return newS;
+            }
+
+            template <typename S>
+            Weight& operator=(S s)
+            {
+                Weight<double> newS(1000);
+                return newS;
+            }
+
+            template <typename S>
+            Weight& operator*(S s)
+            {
+                Weight<double> newS(1000);
+                return newS;
+            }
+
+            template <typename S>
+            bool operator==(const S& rhs)
+            {
+                return true;
+            }
+
+            template <typename S>
+            bool operator<=(S s)
+            {
+                return true;
+            }
+
+            template <typename S>
+            bool operator<(S s)
+            {
+                return true;
+            }
+
+            template <typename S>
+            bool operator>=(S s)
+            {
+                return true;
+            }
+
+            template <typename S>
+            bool operator>(S s)
+            {
+                return true;
+            }
+
+            template <typename S>
+            bool operator!=(S s)
+            {
+                return true;
+            }
     };
 
-    using microgram = weight<int , std::ratio<1, 1000>>;
-    using kilogram = weight<int , std::ratio<1000, 1>>;
-    using gram = weight<int , std::ratio<1, 1>>;
-    using ton = weight<int , std::ratio<110231, 100000000000>>;
-    using ounce = weight<int , std::ratio<35274, 1000000>>;
-    using pound = weight<int , std::ratio<220462, 100000000>>;
+    template <typename T, typename W>
+    T weight_cast(W w)
+    {
+        T t;
+        // math
+        return t;
+    }
+
+    using microgram = Weight<double , std::ratio<1, 1000>>;
+    using kilogram = Weight<double , std::ratio<1000, 1>>;
+    using gram = Weight<double , std::ratio<1, 1>>;
+    using ton = Weight<double , std::ratio<110231, 100000000000>>;
+    using ounce = Weight<double , std::ratio<35274, 1000000>>;
+    using pound = Weight<double , std::ratio<220462, 100000000>>;
+
+}
+
+template <typename S>
+S operator*(double i, const S& s)
+{
+    return s;
 }
